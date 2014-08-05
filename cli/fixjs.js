2555bde47029df027c58358e5ac7c5b31c4db20f
ishowshao <ishowshao@me.com>
2014-08-05 17:46:47 +0800

    format test

--- cli/fixjs.js
+++ cli/fixjs.js
@@ -13,24 +13,39 @@
  * edp seed
  */
 exports.cli = {
-    options: ['replace'],
+    options: [
+        'replace'
+    ],
     description: 'edp fix js style',
     main: function (args, opts) {
-        console.log('Arguments = ' + JSON.stringify(args));
-        console.log('Options = ' + JSON.stringify(opts));
-        var patterns = [
-            '**/*.js',
-            '!**/{output,test,node_modules,asset,dist,release,doc,dep,report}/**'
-        ];
-
-        var candidates = require('../lib/util').getCandidates(args, patterns);
-
-        if (candidates.length) {
-            candidates.forEach(function (path) {
-                console.log(require('jformatter').formatFile(path));
+        var fs = require('fs');
+        if (args[0] === 'clean') {
+            require('../lib/util').getCandidates([], [
+                '**/*.formatted.js',
+                '!**/{output,test,node_modules,asset,dist,release,doc,dep,report}/**'
+            ]).forEach(function (path) {
+                console.log(path + ' removed.');
+                fs.unlinkSync(path);
             });
+        } else {
+            var patterns = [
+                '**/*.js',
+                '!**/{output,test,node_modules,asset,dist,release,doc,dep,report}/**'
+            ];
+            var candidates = require('../lib/util').getCandidates(args, patterns);
+            if (candidates.length) {
+                candidates.forEach(function (path) {
+                    var log = path;
+                    var formattedString = require('jformatter').formatFile(path);
+                    if (!opts.replace) {
+                        path = path.substr(0, path.length - 2) + 'formatted.js';
+                        log += ' --> ' + path;
+                    }
+                    fs.writeFileSync(path, formattedString);
+                    console.log(log + ' formatted.');
+                });
+            }
         }
     }
 };
-
 /* vim: set ts=4 sw=4 sts=4 tw=100: */
--- cli/fixjs/foo.js
+++ cli/fixjs/foo.js
@@ -14,11 +14,13 @@
  */
 exports.cli = {
     description: '这个是必须的，用来简单的描述命令所做的事情',
-    options: ['hello', 'world:'],
+    options: [
+        'hello',
+        'world:'
+    ],
     main: function (args, opts) {
         console.log('Arguments = ' + JSON.stringify(args));
         console.log('Options = ' + JSON.stringify(opts));
     }
 };
-
 /* vim: set ts=4 sw=4 sts=4 tw=100: */
