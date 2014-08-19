476738b8ad5208e10f63376a14c3b5c672b5c9a1
ishowshao <ishowshao@me.com>
2014-08-19 11:58:00 +0800

    引入fixmyjs

--- cli/fixjs.js
+++ cli/fixjs.js
@@ -39,16 +39,87 @@ exports.cli = {
             if (candidates.length) {
                 candidates.forEach(function (path) {
                     var log = path;
-                    var formattedString = require('jformatter').formatFile(path);
+                    var sourceCode = require('fs').readFileSync(path, 'utf-8');
+                    var fixedCode = require('fixmyjs').fix(sourceCode, jshintConfig);
+                    var formattedCode = require('jformatter').format(fixedCode);
                     if (!opts.replace) {
                         path = path.substr(0, path.length - 2) + 'formatted.js';
                         log += ' --> ' + path;
                     }
-                    fs.writeFileSync(path, formattedString);
+                    fs.writeFileSync(path, formattedCode);
                     console.log(log + ' formatted.');
                 });
             }
         }
     }
+};
+
+/* jslint quotmark: true */
+var jshintConfig = {
+    "indent": 4, //fixmyjs 依赖这个配置，不然生成的代码indent == 2
+    "bitwise": false,
+    "camelcase": true,
+    "curly": true,
+    "es3": true,
+    "eqeqeq": false,
+    "forin": false,
+    "immed": true,
+    "latedef": false,
+    "newcap": true,
+    "noarg": false,
+    "noempty": true,
+    "nonew": true,
+    "plusplus": false,
+    "quotmark": "single",
+    "regexp": false,
+    "undef": false,
+    "unused": "vars",
+    "strict": false,
+    "trailing": false,
+    "maxparams": 20,
+    "maxdepth": 6,
+    "maxlen": 120,
+
+    "asi": false,
+    "boss": false,
+    "debug": false,
+    "eqnull": true,
+    "esnext": false,
+    "evil": true,
+    "expr": true,
+    "funcscope": false,
+    "globalstrict": false,
+    "iterator": false,
+    "lastsemic": false,
+    "laxbreak": true,
+    "laxcomma": false,
+    "loopfunc": false,
+    "multistr": false,
+    "onecase": false,
+    "proto": false,
+    "regexdash": false,
+    "scripturl": false,
+    "smarttabs": false,
+    "shadow": true,
+    "sub": true,
+    "supernew": false,
+    "validthis": true,
+
+    "browser": true,
+    "couch": false,
+    "devel": true,
+    "dojo": false,
+    "jquery": true,
+    "mootools": false,
+    "node": false,
+    "nonstandard": false,
+    "prototypejs": false,
+    "rhino": false,
+    "wsh": false,
+
+    "nomen": false,
+    "onevar": false,
+    "passfail": false,
+    "white": false
 };
 /* vim: set ts=4 sw=4 sts=4 tw=100: */
